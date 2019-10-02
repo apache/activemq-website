5b013e7aefbddfe794fe0372acb76ad49cd3a94b
Clebert Suconic <clebertsuconic@apache.org>
2019-10-02 16:46:22 -0400

    update build to use a single branch
    That is the output of this build will go towards ./content instead of checking out another branch

--- build.sh
+++ build.sh
@@ -1,24 +1,4 @@
 set -e
-echo "Building ActiveMQ site (output is in ./site-content) ..."
-jekyll build -s ./src/ -d ./site-content
-COMMIT_HASH=`git rev-parse HEAD`
-COMMIT_MESSAGE=`git show -s --format=%s HEAD`
-
-git remote add github-upstream https://github.com/apache/activemq-website.git
-
-echo "Build of ActiveMQ site successful."
-git fetch github-upstream
-git checkout github-upstream/asf-site -B asf-site
-rm -rf content
-mv site-content content
-git add content
-git remote remove github-upstream
-
-echo "Commiting changes to asf-site branch from master branch."
-git commit --author "ActiveMQ Dev <dev@activemq.apache.org>" -m "$COMMIT_MESSAGE / built from commit $COMMIT_HASH"
-
-echo "-----------------------------------------------------"
-echo "SITE BUILD SUCCESSFUL. You are now on the asf-site branch."
-echo "Run git push origin asf-site to update the live site."
+jekyll build --incremental -s ./src/ -d ./content
 set +e
 
