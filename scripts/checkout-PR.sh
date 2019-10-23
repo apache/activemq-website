a111afc00c45cba1efd315e2faa50df33e348b1b
Clebert Suconic <clebertsuconic@apache.org>
2019-10-23 09:49:59 -0400

    adding merge scripts similar to artemis (checkout a PR, merge a PR and merge a checked out PR)

---
+++ scripts/checkout-PR.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+# Setting the script to fail if anything goes wrong
+set -e
+
+# this script is a helper that will checkout the PR Branch
+
+ACTIVEMQWEB_USER_REMOTE_NAME=${ACTIVEMQWEB_USER_REMOTE_NAME:-origin}
+ACTIVEMQWEB_APACHE_REMOTE_NAME=${ACTIVEMQWEB_APACHE_REMOTE_NAME:-apache}
+ACTIVEMQWEB_GITHUB_REMOTE_NAME=${ACTIVEMQWEB_GITHUB_REMOTE_NAME:-upstream}
+
+git fetch $ACTIVEMQWEB_USER_REMOTE_NAME
+git fetch $ACTIVEMQWEB_APACHE_REMOTE_NAME
+git fetch $ACTIVEMQWEB_GITHUB_REMOTE_NAME
+
+git checkout $ACTIVEMQWEB_GITHUB_REMOTE_NAME/pr/$1 -B $1
+
+echo "\ndo your own rebase by typing: git pull --rebase $ACTIVEMQWEB_APACHE_REMOTE_NAME master"
---
+++ scripts/merge-PR.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+# Setting the script to fail if anything goes wrong
+set -e
+
+# Use this to simplify the rebasing of PRs. PRs will be rebased during the merge on this process.
+# use: ./merge-PR <PRID> textual description
+
+# this script assumes the following remote entries on your config
+#
+# - origin being your github fork:: https://github.com/YOU/activemq-ACTIVEMQWEB.git
+# - upstream being the github fork for apache:: https://github.com/apache/activemq-ACTIVEMQWEB.git
+# - apache being the apache origin:: https://gitbox.apache.org/repos/asf/activemq-ACTIVEMQWEB.git
+#
+# Notice: you should add +refs/pull/*/head to your fetch config on upstream
+#        as specified on https://github.com/apache/activemq-ACTIVEMQWEB/blob/master/docs/hacking-guide/en/maintainers.md
+
+ACTIVEMQWEB_USER_REMOTE_NAME=${ACTIVEMQWEB_USER_REMOTE_NAME:-origin}
+ACTIVEMQWEB_APACHE_REMOTE_NAME=${ACTIVEMQWEB_APACHE_REMOTE_NAME:-apache}
+ACTIVEMQWEB_GITHUB_REMOTE_NAME=${ACTIVEMQWEB_GITHUB_REMOTE_NAME:-upstream}
+
+git fetch $ACTIVEMQWEB_USER_REMOTE_NAME
+git fetch $ACTIVEMQWEB_APACHE_REMOTE_NAME
+git fetch $ACTIVEMQWEB_GITHUB_REMOTE_NAME
+
+git checkout $ACTIVEMQWEB_APACHE_REMOTE_NAME/master -B master
+git checkout $ACTIVEMQWEB_GITHUB_REMOTE_NAME/pr/$1 -B $1
+git pull --rebase $ACTIVEMQWEB_APACHE_REMOTE_NAME master
+git checkout master
+git merge --no-ff $1 -m "This closes #$*"
+git branch -D $1
